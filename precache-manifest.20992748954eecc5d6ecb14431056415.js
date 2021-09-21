d76f6025a4a9bd14d6c0e74b1d3ab48afc4a8fba
root <phat.dangthanh@pharmacity.vn>
2021-09-21 17:44:43 +0700

    Updates

---
+++ precache-manifest.20992748954eecc5d6ecb14431056415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4413c2ffea7ccf4766c9d4e0e412fa",
+    "url": "/random-password-generator/index.html"
+  },
+  {
+    "revision": "74e10433d12b601eed9e",
+    "url": "/random-password-generator/static/css/2.53d98138.chunk.css"
+  },
+  {
+    "revision": "52bbbca4ea8dd2bbb425",
+    "url": "/random-password-generator/static/css/main.39b22fcc.chunk.css"
+  },
+  {
+    "revision": "74e10433d12b601eed9e",
+    "url": "/random-password-generator/static/js/2.8e8503f7.chunk.js"
+  },
+  {
+    "revision": "52bbbca4ea8dd2bbb425",
+    "url": "/random-password-generator/static/js/main.88aead2d.chunk.js"
+  },
+  {
+    "revision": "6a4e8e983455ad29337c",
+    "url": "/random-password-generator/static/js/runtime-main.0c5dc5a3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/random-password-generator/precache-manifest.20992748954eecc5d6ecb14431056415.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-password-generator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
