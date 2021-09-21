2ce90f428d451823c4fbd3737c609abc34f44f9e
root <phat.dangthanh@pharmacity.vn>
2021-09-21 19:44:14 +0700

    Updates

--- precache-manifest.664a5446de1bfbc2f9806c49138bf1ce.js
+++ precache-manifest.664a5446de1bfbc2f9806c49138bf1ce.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "05c0b04b02834bbbead09b1f15b35634",
+    "revision": "117bead5cc90640d4d28ddaf239cbcef",
     "url": "/random-password-generator/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/random-password-generator/static/css/2.53d98138.chunk.css"
   },
   {
-    "revision": "d2f6c1fb089839b57123",
+    "revision": "a22a60999efad4476dd6",
     "url": "/random-password-generator/static/css/main.39b22fcc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/random-password-generator/static/js/2.8e8503f7.chunk.js"
   },
   {
-    "revision": "d2f6c1fb089839b57123",
-    "url": "/random-password-generator/static/js/main.5d90cf1b.chunk.js"
+    "revision": "a22a60999efad4476dd6",
+    "url": "/random-password-generator/static/js/main.677bf479.chunk.js"
   },
   {
     "revision": "6a4e8e983455ad29337c",
